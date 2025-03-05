b0a2aa3625eef51a433d908e9bb2d72267a6120e
georgeCentennial <gcayaby2@my.centennialcollege.ca>
2025-03-05 04:39:13 -0500

    modified remove many

--- server/routes/contact.routes.js
+++ server/routes/contact.routes.js
@@ -4,7 +4,7 @@ import contactCtrl from '../controllers/contact.controller.js'
 const router = express.Router()
 router.route('/api/contacts').post(contactCtrl.create)
 router.route('/api/contacts').get(contactCtrl.list)
-router.route('/api/contacts').get(contactCtrl.list)
+router.route('/api/contacts').delete(contactCtrl.removeMany)
 router.param('contactId', contactCtrl.contactByID)
 router.route('/api/contacts/:contactId').get(contactCtrl.read)
 router.route('/api/contacts/:contactId').put(contactCtrl.update)
